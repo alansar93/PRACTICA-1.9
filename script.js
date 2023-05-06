6bed412ae2adaf757926207a74bfce2a645fd3d3
Alan Sarchman <alansarchman@live.com.ar>
2023-05-06 18:54:19 -0300

    realizado con dudas al final

---
+++ script.js
@@ -0,0 +1,8 @@
+const CUERPO = document.querySelector("body");
+
+function cambios() {
+    CUERPO.style.color = "black"
+    CUERPO.style.backgroundColor = "white"
+    CUERPO.style.fontSize = "35px"
+    CUERPO.style.fontFamily = "arial"
+}
